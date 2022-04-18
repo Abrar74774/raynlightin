a98ff64e7b37eef04b32d456ee624c280178911c
abrar-ah1912955 <ah1912955@qu.edu.qa>
2022-04-18 14:49:44 +0300

    navbar modified - now hides on scroll down on mobile

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -1,18 +1,23 @@
-import { useEffect } from "react/cjs/react.development"
+import { useEffect, useState } from "react/cjs/react.development"
 import styles from "./styles/Navbar.module.css"
 
 
 export default function Navbar(props) {
-    
+    const [away, setAway] = useState(false);
+    const [prevScroll, setPrevScroll] = useState(0);
     const handleScroll = (e) => {
-        console.log(this)
+        const currentScroll = window.pageYOffset;
+        console.log(currentScroll)
+        if (currentScroll > prevScroll) setAway(true);
+        else setAway(false);
+        setPrevScroll(currentScroll); // TO DO : Fix finicky navbar : Maybe DOne???
     }
     useEffect(() => {
         window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll',handleScroll);
     })
     return (
-        <div className={styles["nav-container"]}>
+        <div className={styles["nav-container"]} style={{transform: away? 'translateY(-100%)' : null}}>
             <div className={styles.logo}>Ray_N</div>
             <div className={styles.hamburger}>
                 <div></div>
