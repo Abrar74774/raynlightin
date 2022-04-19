1d749cc6e2a2b9394469e1c9bd4145c3e660c199
abrar-ah1912955 <ah1912955@qu.edu.qa>
2022-04-19 10:42:57 +0300

    added navigation menus to navbar

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -5,25 +5,38 @@ import styles from "./styles/Navbar.module.css"
 export default function Navbar(props) {
     const [away, setAway] = useState(false);
     const [prevScroll, setPrevScroll] = useState(0);
+    const [menuOpen, setMenuOpen] = useState(false);
+
     const handleScroll = (e) => {
         const currentScroll = window.pageYOffset;
-        console.log(currentScroll)
         if (currentScroll > prevScroll) setAway(true);
         else setAway(false);
         setPrevScroll(currentScroll); // TO DO : Fix finicky navbar : Maybe DOne???
     }
+
     useEffect(() => {
         window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll',handleScroll);
     })
     return (
-        <div className={styles["nav-container"]} style={{transform: away? 'translateY(-100%)' : null}}>
-            <div className={styles.logo}>Ray_N</div>
-            <div className={styles.hamburger}>
-                <div></div>
-                <div></div>
-                <div></div>
+        <div className={`${styles["nav-container"]} ${menuOpen? styles["nav-menu"]: ""}`} style={{transform: away? 'translateY(-100%)' : null}}>
+            <div className="flex">
+                <div className={styles.logo}>Ray_N</div>
+                <div className={styles.hamburger} onClick={() => setMenuOpen(!menuOpen)}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
             </div>
+            {menuOpen &&
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">View</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+            }
         </div>
     )
 }
