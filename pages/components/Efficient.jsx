7674ca465d14405134f8c2831f34a9a76f49f716
abrar-ah1912955 <ah1912955@qu.edu.qa>
2022-04-20 17:30:52 +0300

    changed footer and navbar a bit

--- pages/components/Efficient.jsx
+++ pages/components/Efficient.jsx
@@ -14,7 +14,7 @@ export default function Efficient(props) {
                 />
             </div>
             <div>
-                <h2>Environment Safe</h2>
+                <h2>Efficient</h2>
                 <p>
                     RayN manufactures the best Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum obcaecati voluptatem in, delectus deleniti assumenda quidem tempora labore aspernatur facere maiores amet quibusdam. Inventore assumenda obcaecati voluptate, enim sunt numquam.
                 </p>
--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -4,11 +4,12 @@ export default function Footer(props) {
     return (
         <footer className={styles.container}>
             <div className={styles.contact}>
-                <h1>Contact</h1>
-                <a href="mailto:abrarshahriarhossain@gmail.com">email@email.com</a>
+                <h1>Follow</h1>
+                <a href="#">Twitter</a>
                 <a href="#">Facebook</a>
                 <a href="#">Instagram</a>
-                <a href="#">Twitter</a>
+                <h1>Contact</h1>
+                <a href="mailto:abrarshahriarhossain@gmail.com">email@email.com</a>
             </div>
             <div className={styles.by}>
                 Website by&nbsp;
