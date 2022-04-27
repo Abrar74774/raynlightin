a53ecc351d84e685ffc5d851c0795a56bc020290
Abrar74774 <abrarshahriarhossain@gmail.com>
2022-04-27 23:57:23 +0300

    some styling changes

--- pages/components/LongLasting.jsx
+++ pages/components/LongLasting.jsx
@@ -1,6 +1,3 @@
-import styles from './styles/LongLasting.module.css'
-import lamp from '../../public/lamp_simple.jpg'
-import Image from 'next/image'
 import PicDesc from './PicDesc'
 
 export default function LongLasting(props) {
@@ -15,6 +12,7 @@ export default function LongLasting(props) {
                     <p>
                        Reliability is our best Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum obcaecati voluptatem in, delectus deleniti assumenda quidem tempora labore aspernatur facere maiores amet quibusdam. Inventore assumenda obcaecati voluptate, enim sunt numquam.
                    </p>
+                   <button className='view'>Browse Products</button>
                 </>
             }
         />
